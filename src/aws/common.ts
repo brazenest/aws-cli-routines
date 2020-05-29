34860f8b23a5e93e260af5e62a1e38b08365e390
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2020-05-29 14:55:31 -0700

    Parallelize execution of AWS CLI commands

--- src/aws/common.ts
+++ src/aws/common.ts
@@ -1,11 +1,14 @@
-import { execSync } from 'child_process'
+import util from 'util'
+import { exec } from 'child_process'
 import logger from '../logging.js'
 
 import type { AwsStatement, AwsResponse } from '../types/aws/common.js'
 
+const execAsync = util.promisify(exec)
+
 export const executeStatement = async (statement: AwsStatement): Promise<AwsResponse> => {
   const statementAsString = statement.toString()
   logger.info(`Executing ${statementAsString} ...`)
-  const response = execSync(statementAsString, { encoding: 'utf8' })
+  const { stdout: response } = await execAsync(statementAsString, { encoding: 'utf8' })
   return (response.length) ? JSON.parse(response) : response
 }
